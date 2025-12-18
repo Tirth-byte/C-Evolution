d298c6d74ee7a1e6a5052f64dd5712b5a721c23c
TIRTH PATEL <kavartirth9@gmail.com>
2025-12-18 13:38:49 +0530

    Completed Chapter 2 Problem
    Solved all practice problems from Chapter 2
    Covered basic C concepts such as data types, operators, expressions, and type conversion
    Verified outputs and logic for each solution
    Code is organized and ready for review

---
+++ Ch-2_problem.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    printf("Welcome !\n");
+    int num;
+    printf("Enter the number you want to check for deviceble by 97 or not\n");
+   scanf("%d",&num);
+   if(num % 97 == 0){
+    printf("%d is divisible by 97\n",num);
+   }
+   else{
+    printf("%d is not divisible by 97\n",num);
+   }
+   return 0;
+}
